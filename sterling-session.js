fb798c2245e6d5f44ccdfb47909f8f5474ceaf70
khrome <abbey@khrome.net>
2017-08-21 19:38:18 -0700

    fixes

--- sterling-session.js
+++ sterling-session.js
@@ -22,8 +22,9 @@
         serve : function(sterlingInstance, datasource, authFunction){
             var controls = {
                 handleRequest : function(id, req, res, cb){
+                    var query = 'SELECT * from Session where session = "'+id+'"'
                     datasource.query(
-                        'SELECT * from session where session = "'+id+'"',
+                        query,
                         function(error, results, fields){
                             if(error || (!results) || (!results[0])){
                                 return cb(new Error('No User, No Session'));
@@ -33,13 +34,13 @@
                                 var user;
                                 req.user = function(cb){
                                     if(user) return cb(undefined, user);
-                                    try{
-                                        authFunction(req.post || (
-                                            SterlingSession.allowGet && req.get
-                                        ), cb);
-                                    }catch(ex){
-                                        cb(ex);
-                                    }
+                                    //try{
+                                        authFunction({id:session.userid}, function(err, user){
+                                            cb(err, user)
+                                        });
+                                    //}catch(ex){
+                                        //cb(ex);
+                                    //}
                                 };
                                 cb(undefined);
                             }
@@ -48,7 +49,7 @@
                 },
                 deleteSession : function(id, cb){
                     datasource.query(
-                        'DELETE from session where session = "'+id+'"',
+                        'DELETE from Session where session = "'+id+'"',
                         function(error, results, fields){
                             cb(error);
                         }
@@ -58,6 +59,7 @@
                     if(!u) return cb(new Error('User not supplied'))
                     var act = function(user){
                         var id = uuid.v4();
+                        if(!user.id) return cb(new Error('User has no ID'))
                         datasource.query(SQL.save('Session', {
                             userid:user.id,
                             session:id
@@ -70,7 +72,7 @@
                     }
                     if(authFunction){
                         authFunction(u, function(err, user){
-                            if(err) return cb(new Error('User not supplied'))
+                            if(err) return cb(new Error('User not supplied'));
                             act(user);
                         });
                     }else{
@@ -78,24 +80,41 @@
                     }
                 }
             };
-            sterlingInstance.addSecureRoute = function(route, handlers){
-                var wrappedHandlers = {};
-                Object.keys(handlers).forEach(function(method){
-                    var handler = handlers[method];
-                    wrappedHandlers[method] = function(){
-                        var args = Array.prototype.slice.call(arguments);
-                        var ob = this;
-                        controls.handleRequest(ob.res, ob.req, function(){
-                            handler.apply(ob, args);
-                        });
-                    }
+            var secureRoute = function(route, handler){
+                //console.log('SR-a', route);
+                sterlingInstance.addRoute(route, function(){
+                    //console.log('SR-e', arguments);
+                    //the session token is the first arg on the url
+                    var args = Array.prototype.slice.call(arguments);
+                    var session = arguments[0];
+                    var ob = this;
+                    controls.handleRequest(session, ob.res, ob.req, function(err){
+                        if(!ob.res.user){
+                            return sterlingInstance.error(
+                                ob.res, new Error('No Authentication Methods'), 401
+                            );
+                        }
+                        if(err) return sterlingInstance.error(ob.res, err, 401);
+                        handler.apply(ob, args);
+                    });
                 });
-                sterlingInstance.addRoute(route, wrappedHandlers);
             }
+
+            sterlingInstance.addSecureRoute = function(route, handler){
+                if(typeof handler == 'function'){
+                    secureRoute(route, handler);
+                }else{
+                    Object.keys(handler).forEach(function(method){
+                        var lowerMethod = method.toLowerCase();
+                        secureRoute(route, handler[lowerMethod]);
+                    });
+                }
+            }
+
             var prefix = sterlingInstance.prefix || '';
             sterlingInstance.addRoute(prefix+'session/:session', {get:function(session){
                 var ob = this;
-                controls.handleRequest(session, ob.res, ob.req, function(err){
+                controls.handleRequest(session, ob.req, ob.res, function(err){
                     if(err){
                         ob.res.end(JSON.stringify({
                             success: false,
